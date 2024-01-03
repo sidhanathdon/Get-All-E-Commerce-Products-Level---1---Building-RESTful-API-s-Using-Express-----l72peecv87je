c9564af1a56ccf9ca052daf9f89f16867ee06660
sidhanathdon <137877586+sidhanathdon@users.noreply.github.com>
2024-01-03 10:50:07 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,5 +10,17 @@ app.use(express.json());
 
 // Write GET endpoint for sending all the products to client here
 // Endpoint - /api/v1/products
-
+app.get("/api/v1/products",(req,res)=>{
+    if(products.length){
+        res.status(200).json({
+            status:"success",
+            message:"Product fetched successfully",
+            data:{
+                products,
+            },
+        });
+    }else{
+        res.status(404).json({message:"Product not found"});
+    }
+})
 module.exports = app;
